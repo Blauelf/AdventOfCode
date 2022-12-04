74bfbf387454e9b23ee0a987b6a3bc20d4f52dea
Blauelf <5952026+Blauelf@users.noreply.github.com>
2022-12-04 09:41:20 +0100

    Day 4

---
+++ 04/level2.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+import re
+
+
+def main():
+    with open('input.txt') as f:
+        arr = [list(map(int, re.findall("\d+", assignment)))
+               for assignment in f]
+    total = sum(a[0] <= a[3] <= a[1] or a[2] <= a[1] <= a[3] or
+                a[0] <= a[2] <= a[1] or a[2] <= a[0] <= a[3]
+                for a in arr)
+    print(total)
+
+
+if __name__ == '__main__':
+    main()
